28c60e6351f9c790fa7bb7ca2f1734ffa7713b7c
ijasahamedstr <ijasahamed.str@gmail.com>
2024-06-24 04:48:49 -0700

    commit

--- frontend/src/component/Page/social media accounts/social media accounts.js
+++ frontend/src/component/Page/social media accounts/social media accounts.js
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import Slider from "react-slick";
-import Button from 'react-bootstrap/Button';
+
 
 function Soicalaccount() {
   const marginLeftValue = '15px', marginTopValue = '50px',marginBottomValue = '50px';
